65e77a22064e0bcc035c47c4ed77ceae8b7035c2
hameleoncheg <hameleoncheg@gmail.com>
2023-05-03 17:38:07 +0300

    create project

---
+++ src/main/java/go/it/java_notepad/JavaNotepadApplication.java
@@ -0,0 +1,13 @@
+package go.it.java_notepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNotepadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNotepadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/go/it/java_notepad/JavaNotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package go.it.java_notepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNotepadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
