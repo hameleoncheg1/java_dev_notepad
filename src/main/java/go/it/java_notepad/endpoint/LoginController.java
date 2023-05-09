76baff9442a24f3c0a89cbf6c8647d7431eab629
Dmytro Kononenko <rizset290993@gmail.com>
2023-05-09 11:42:32 +0300

    seventh intermediate commit

--- src/main/java/go/it/java_notepad/endpoint/LoginController.java
+++ src/main/java/go/it/java_notepad/endpoint/LoginController.java
@@ -1,6 +1,10 @@
 package go.it.java_notepad.endpoint;
 
 import go.it.java_notepad.entity.User;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,14 +16,25 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-    @GetMapping
-    public ModelAndView login(@RequestParam String username,
+    @PostMapping
+    public RedirectView login(@RequestParam String username,
                                     @RequestParam String password){
 
-        ModelAndView result = new ModelAndView("login");
-        return result;
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication =
+                new TestingAuthenticationToken(username, password, "ROLE_USER");
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+
+
+        return new RedirectView("/note/list");
     }
 
+    @GetMapping
+    public ModelAndView login(){
+                ModelAndView result = new ModelAndView("login");
+        return result;
+    }
 //    @PostMapping("")
 //    public RedirectView registerAdd(@RequestParam String username,
 //                                    @RequestParam String password) {
